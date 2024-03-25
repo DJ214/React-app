bc196503877ff1532f616c382b1c6eb8c9772b23
DJ214 <jaindeepanshu21@gmail.com>
2024-03-25 20:22:48 +0530

    react-basics

---
+++ my-app/src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import UserProfile from './Components/UserProfile';
+import MyComponent from './Components/MyComponent';
+import Counter from './Components/Counter';
+import Product from './Components/Product';
+import Form from './Components/Form';
+import Ue from './Components/Ue';
+
+export default function App() {
+  return (
+    // <div>
+    //   <Header/>
+    //   <Header/>
+    //   <Header/>
+    //   <Header/>
+    //   <Header/>
+    //   <Header/>
+    // </div>
+    
+    <>
+      {/* <Header/>
+      <Header/>
+      <Header/>
+      <Counter/>
+      <UserProfile name="Alex" age="30" occupation="Instructor"/>
+      <UserProfile name="Aman" age="40" occupation="Instructor"/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <UserProfile name="Stokes" age="30" occupation="Cricketer"/>
+      <UserProfile name="Siba" age="No idea" occupation="Developer"/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+
+      <MyComponent/>
+
+      <Footer/> */}
+      {/* <Product/> */}
+      {/* <Form/> */}
+      <Ue/>
+    </>
+  );
+}
+
+
+//named export
+// export function AnotherApp(){
+//   return(
+//     <div>Another App</div>
+//   )
+// }
+
+// export const a = 10;
